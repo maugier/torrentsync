dbbdae40bcef2585791a5226d866d209d8639dc8
Maxime Augier <max@xolus.net>
2009-07-15 09:17:54 +0200

    Group torrent parsing-related functions

---
+++ torrent.erl
@@ -0,0 +1,29 @@
+-module(torrent).
+
+-export([parse/1, multi/1, files/1]).
+
+parse(FileName) ->
+	{ok, TData} = file:read_file(FileName),
+	{MetaInfo, <<>>} = benc:decode(TData),
+	MetaInfo.
+
+multi(MetaInfo) ->
+	Info = proplists:get_value(<<"info">>, MetaInfo),
+	case proplists:is_defined(<<"length">>, Info) of
+		true -> single;
+		_    -> multi
+	end.
+
+files(MetaInfo) ->
+	Info = proplists:get_value(<<"info">>, MetaInfo),
+	Files = proplists:get_value(<<"files">>, Info),
+	BaseName = proplists:get_value(<<"name">>, Info),
+	[ read_file_item(BaseName, Item) || Item <- Files ].
+
+read_file_item(BaseName, Item) ->
+	Len = proplists:get_value(<<"length">>, Item),
+	PathList = proplists:get_value(<<"path">>, Item),
+	Path = string:join([binary_to_list(P) || P <- [BaseName|PathList]] , "/"),
+	{ Path, Len }.
+
+
