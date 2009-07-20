e44aca206a29d9e3c6c7eb3e9b4f8a4fdbcd33f7
Maxime Augier <max@xolus.net>
2009-07-20 11:23:02 +0200

    Making store a gen_server

--- store.erl
+++ store.erl
@@ -1,11 +1,25 @@
 -module(store).
 
--export([write/2]).
+-export([start/1, write/2]).
 
-write(Torrent, { piece, Id, Data }) ->
+-behaviour(gen_server).
+-record(st, {piece_length, files, writer}).
+-export([init/1, handle_call/3, handle_cast/2]).
+
+init([Torrent]) -> 
 	Files = torrent:files(Torrent),
 	PieceLength = torrent:piece_length(Torrent),
-	_Slices = slice_piece(Files, Id * PieceLength, Data).
+	PID = spawn_link(fun writer/0),
+	{ok, #st{piece_length=PieceLength,
+		 files=Files,
+		 writer=PID}}.
+
+
+handle_cast({piece, Id, Data}, ST) ->
+	Slices = slice_piece(ST#st.files, ID * ST#st.piece_length, Data),
+	PID = ST#st.writer,
+	[PID ! S || Slices ],
+	{noreply, ST}.
 
 
 slice_piece(_,_,<<>>) -> [];
@@ -14,10 +28,36 @@ slice_piece([{File,Size}|Tail], Offset, Data) ->
 		slice_piece(Tail, Offset - Size, Data);
 	true ->
 		case (Size - Offset) of N when N > size(Data) ->
-			[{File, Offset, Data}];
+			[{write_piece, File, Offset, Data}];
 		N ->
 			<<Slice:N/binary,Rest/binary>> = Data,
-			[{File, Offset, Slice} |
+			[{write_piece, File, Offset, Slice} |
 			 slice_piece(Tail, 0, Rest) ]
 		end
 	end.
+writer() -> writer(null,null).
+writer(FileName,FD) ->
+	receive 
+	    {write_piece, NewFileName, Offset, Slice} ->
+		if 
+		    FileName == NewFileName ->
+			write_to_fd(FD,Offset,Slice),
+			writer(FileName,FD);
+		    true ->
+		    	ok = case FD of null -> ok; _ -> file:close(FD) end,
+			case file:open(NewFileName,[write,binary]) of
+			    {ok, NewFD} ->
+			    	write_to_fd(NewFD,Offset,Slice),
+				writer(NewFileName, NewFD);
+			    Err -> 
+				error_logger:error_report
+					([opening_fd, {err,Err}]),
+				writer(null,null)
+			end
+	        end;
+	    done ->
+	        ok
+end.
+
+write_to_fd(FD, Offset, Slice) ->
+	ok = file:pwrite(FD,[{{bof,Offset},Slice}]).
