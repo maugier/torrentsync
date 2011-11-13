015ea63dcf0c1fd1593ee17f35635243640b6901
Maxime Augier <max@xolus.net>
2011-11-13 02:16:46 +0100

    Comments

--- loader.erl
+++ loader.erl
@@ -24,22 +24,43 @@ load(Torrent,Base,Dest) ->
 
 
 %%%%%%%%%%%%%%%%%
-% Load the pieces data from a torrent
+% Load the pieces data from a torrent, given a list of files and piece size
 load_pieces(Dest,FileList,PSize) -> load_pieces(Dest,FileList,PSize,0,<<>>).
 
-load_pieces(Dest,[],_,Cnt,Last) ->
-	case size(Last) of 0 -> ok; _ -> Dest ! {piece,Cnt,Last}, ok end;
+%
+% load_pieces(Dest,Files,PS,Cnt,Last)
+% Dest = process of recipient
+% Files = list of {Filename, Size}
+% PS = piece size
+% Cnt = piece index
+% Last = fragment of data read from last file
 
+
+% no more files, create last undersized tail piece
+load_pieces(Dest,[],_,Cnt,Last) ->
+	case size(Last) of 
+		0 -> ok; 
+		_ -> Dest ! {piece,Cnt,Last}, ok %send 
+	end;
+
+% read from a new file
 load_pieces(Dest,[{File,Len}|FT],PS,Cnt,Last) -> 
 	{ok, FD} = pad_read:open(File,Len),
 	{ok, NewCnt, NewLast} = load_pieces_file(Dest,FD,PS,Cnt,Last),
 	ok = pad_read:close(FD),
 	load_pieces(Dest,FT,PS,NewCnt,NewLast).
 
+% read piece data from a file, return {ok, NewCnt, NewLast}
+% NewCnt = new piece index
+% NewLast = new incomplete piece at end of file
 load_pieces_file(Dest,FD,PS,Cnt,Last) ->
+	
+	%how much we need for a complete piece
 	ToRead = PS - size(Last),
+
 	{ok, Data} = pad_read:read(FD,ToRead),
 	if size(Data) < ToRead ->
+		% file was too short to make a complete file
 		{ok, Cnt, <<Last/binary,Data/binary>>};
 	true -> 
 		Dest ! {piece, Cnt, <<Last/binary,Data/binary>>},
--- pad_read.erl
+++ pad_read.erl
@@ -2,7 +2,7 @@
 
 -export([open/2, read/2, close/1]).
 
-
+% returns {ok, PID} 
 open(Filename,Expect) ->
 	case file:open(Filename, [binary,read]) of
 		{ok, FD} -> 
@@ -16,10 +16,15 @@ close(PID) ->
 	PID ! close,
 	ok.
 
+% try to read Len bytes from the file. If the expected size is smaller that what was requested,
+% pad with zeroes.
+% returns { ok, Data }, or "eof".
 read(PID, Len) ->
 	PID ! { self(), read, Len },
 	receive { PID, Reply } -> Reply end.
 
+
+% read bytes from a file with Expect remaining bytes
 reader(FD,Expect) -> receive
 	close ->
 		ok = file:close(FD);
@@ -27,26 +32,26 @@ reader(FD,Expect) -> receive
 		case file:read(FD, Len) of
 		{ ok, Data } -> 
 			Read = size(Data),
-		 	if Read == Len ->
+		 	if Read == Len -> % we managed to read what was requested
 				PID ! { self(), {ok, Data} },
 				reader(FD, Expect - Len);
-			Len < Expect ->
-				Pad = 8*(Len - Read),
-				PID ! { self(), {ok, <<Data/binary,0:Pad>>} },
+			Len < Expect -> % the read was not supposed to reach eof. Pad up to Len.
+				Pad = Len - Read,
+				PID ! { self(), {ok, <<Data/binary,0:Pad/unit:8>>} },
 				ok = file:close(FD),
 				reader_eof(Expect - Len);
-			true -> 
-				Pad = 8*(Expect - Read),
-				PID ! { self(), {ok, <<Data/binary,0:Pad>>} },
+			true -> % the read was supposed to reach eof. Pad up to expected file length.
+				Pad = Expect - Read,
+				PID ! { self(), {ok, <<Data/binary,0:Pad/unit:8>>} },
 				ok = file:close(FD),
 				real_eof()
 			end;
 		eof -> 
 			ok = file:close(FD),
-			if Len < Expect ->
+			if Len < Expect -> % we expected more than Len, pad up to Len
 				PID ! { self(), {ok, <<0:Len/unit:8>>} },
 				reader_eof(Expect - Len);
-			true ->
+			true -> % expected the file to fall short, only pad up to expected length.
 				PID ! { self(), {ok, <<0:Expect/unit:8>>} },
 				real_eof()
 			end;
@@ -55,6 +60,7 @@ reader(FD,Expect) -> receive
 		end
 	end.
 
+% from now on, only return blocks of zeroes as the file was cut short.
 reader_eof(Expect) -> receive
 	close -> ok;
 	{PID, read, Len} ->
@@ -66,7 +72,7 @@ reader_eof(Expect) -> receive
 			reader_eof(Expect - Len)
 		end
 	end.
-
+% expected eof has been reached, from now on only return eof.
 real_eof() -> receive
 	close -> ok;
 	{PID, read, _Len} -> 
